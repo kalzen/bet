94ac98ec001770ddfa35b0ef0383e6d73a63aeae
Catty <ducvu73@gmail.com>
2024-09-23 11:03:42 +0700

    feat: Adjust spacing for post details

--- resources/views/post/detail.blade.php
+++ resources/views/post/detail.blade.php
@@ -11,7 +11,7 @@
 @section('content')
 <div class="content-wrap ">
         <div class="container">
-            <div class="row pt-60 pb-60">
+            <div class="row pt-5 pb-5">
                 <div class="col-12 col-lg-8 blog-detail">
                     <div class="subtitle mb-15 font-size-15 fw-medium text-gray-dark">{{$post->categories->first()->name}} <!--<span class="sep-dot"></span> {{date('d/m/Y',strtotime($post->updated_at))}} --></div>
                     <h1 class="mb-20">{{$post->title}}</h1>
