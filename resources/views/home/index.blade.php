8916d15fc274234f0462d6b58c92f50ae0e29c9a
Catty <ducvu73@gmail.com>
2024-10-07 11:00:04 +0700

    feat: Improve mobile responsiveness of home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -171,6 +171,7 @@
                                                             class="badge bg-warning text-dark mb-1 mt-1">{{ $post->categories->first()->name }}</span>
                                                         <h6 class="card-title">
                                                             <a href="{{ route('post.detail', ['alias' => $post->slug]) }}">{{ $post->title }}</a>
+                                                            <span class="small-post-description">{{ Str::limit($post->description, 130, '...') }}</span>
                                                         </h6>
                                                         <p class="card-text">
                                                             <small class="text-muted">
@@ -270,6 +271,7 @@
                                                         class="badge bg-warning text-dark mb-1 mt-1">{{ $post->categories->first()->name }}</span>
                                                     <h6 class="card-title">
                                                         <a href="{{ route('post.detail', ['alias' => $post->slug]) }}">{{ $post->title }}</a>
+                                                        <span class="small-post-description">{{ Str::limit($post->description, 130, '...') }}</span>
                                                     </h6>
                                                     <p class="card-text">
                                                         <small class="text-muted">
