d952e7ff56c7992f382ec4b0d7ee4225042f4b82
Catty <ducvu73@gmail.com>
2024-10-09 10:23:46 +0700

    feat: Add "No tips" message

--- resources/lang/en/tip.php
+++ resources/lang/en/tip.php
@@ -7,5 +7,6 @@ return [
         "today_tips" => __('home.today_tips'),
         "prediction" => __('home.prediction'),
         "bet" => __('home.bet'),
+        "no_tips" => __('home.no_tips')
     ]
 ];
