2bc6cc453f0b2670dc14c0676424f18e6aa29578
Catty <ducvu73@gmail.com>
2024-10-14 15:39:08 +0700

    feat: Remove unused language options

--- config/locales.php
+++ config/locales.php
@@ -4,11 +4,5 @@ $locales = [
     'en-US' => 'English (US)',
     'pt-BR' => 'Português (Brazil)',
     'vi-VN' => 'Tiếng Việt (Vietnam)',
-    'zh-CN' => '简体中文 (China)', 
-    'ja-JP' => '日本語 (Japan)',   
-    'es-ES' => 'Español (Spain)',  
-    'ru-RU' => 'Русский (Russia)', 
-    'de-DE' => 'Deutsch (Germany)',
-    // Add more languages here
 ];
 return $locales;
