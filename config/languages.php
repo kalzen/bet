fc9de7ac974c8711b499f0fe1b688f3677f4e9f9
Catty <ducvu73@gmail.com>
2024-10-14 16:07:36 +0700

    feat: Add template file for locales

---
+++ config/languages.php
@@ -0,0 +1,8 @@
+<?php
+// Notice: This is just a template file
+$lang = [
+    'th-TH' => 'ไทย (Thailand)',
+    'en-US' => 'English (US)',
+    'pt-BR' => 'Português (Brazil)',
+    'vi-VN' => 'Tiếng Việt (Vietnam)',
+];
