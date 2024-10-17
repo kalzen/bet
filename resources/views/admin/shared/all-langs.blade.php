da6fd19e6281137b165b9b3486cc6170a5f278b4
Catty <ducvu73@gmail.com>
2024-10-17 17:55:00 +0700

    Fix: Remove default selection for the language dropdown

--- resources/views/admin/shared/all-langs.blade.php
+++ resources/views/admin/shared/all-langs.blade.php
@@ -2,13 +2,9 @@
     data-allow-clear="true">
     <option value="">Chọn ngôn ngữ</option>
     @if (isset($formLangs))
-        @foreach ($formLangs as $key => $lang)
+        @foreach ($formLangs as $lang)
             <option value="{{ $lang?->id }}"
-                {{ (isset($record['lang_id']) && $record['lang_id'] == $lang?->id) || 
-                   (old('lang_id') == $lang?->id) || 
-                   ($key === 0 && !isset($record['lang_id']) && !old('lang_id')) ? 'selected' : '' }}>
-                {{ $lang?->name }}
-            </option>
+                {{ (isset($record['lang_id']) && $record['lang_id'] == $lang?->id) || (old('lang_id') == $lang?->id) ? 'selected' : '' }}>{{ $lang?->name }}</option>
         @endforeach
     @endif
 </select>
