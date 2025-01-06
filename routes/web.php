dd74fe03518c5a8883c50414cab2df96a451b459
kalzen <manhto109@gmail.com>
2025-01-06 22:29:19 +0700

    fixx

--- routes/web.php
+++ routes/web.php
@@ -150,7 +150,9 @@ Route::middleware(['auth'])->name('admin.')->prefix('admin')->group(function () 
 });
 
 // Default locale routes - no prefix
-Route::middleware(['localization'])->group(function () {
+Route::group([
+    'middleware' => ['localization', 'defaultLocale']
+], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/home', [HomeController::class, 'index'])->name('index');
     Route::get('/tip', [ClientTipController::class, 'index'])->name('tip.list');
@@ -164,7 +166,7 @@ Route::middleware(['localization'])->group(function () {
     Route::get('/news/{alias}', [App\Http\Controllers\PostController::class, 'detail'])->name('post.detail');
     Route::any('/livescore', [App\Http\Controllers\LiveScoreController::class, 'index'])->name('livescore.index');
     Route::any('/leaderboard', [App\Http\Controllers\BXHController::class, 'index'])->name('bxh.index');
-})->middleware('defaultLocale');
+});
 
 // Non-default locale routes - with prefix
 Route::group([
