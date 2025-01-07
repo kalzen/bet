7d53b9e05c08a9c6b096312ac5c7fcd1f0aa8243
Catty <ducvu73@gmail.com>
2025-01-07 16:37:57 +0700

    Adds multi-language support for tips

--- app/Models/Tip.php
+++ app/Models/Tip.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\Admin\SharedHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,23 @@ class Tip extends Model
 {
     protected $guarded = [];
     use HasFactory;
+    public function getAvailableLang()
+    {
+        return SharedHelper::getAvailableLang($this);
+    }
+
+    public function langs()
+    {
+        return $this->belongsTo(Lang::class, 'lang_id');
+    }
+
+    public function langParent()
+    {
+        return $this->belongsTo(Tip::class, 'lang_parent_id');
+    }
+    
+    public function langChildren()
+    {
+        return $this->hasMany(Tip::class, 'lang_parent_id');
+    }
 }
