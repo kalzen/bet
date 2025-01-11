30cf576584becb917acea091e765561fd22929b0
Catty <ducvu73@gmail.com>
2025-01-11 18:21:10 +0700

    feat: Add AssignedTitle model with relationships and status constants

---
+++ app/Models/AssignedTitle.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Controllers\Admin\SharedHelper;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class AssignedTitle extends Model
+{
+    const STATUS_ACTIVE = 1;
+    const STATUS_INACTIVE = 0;
+    use HasFactory;
+
+    protected $fillable = ['lang_parent_id', 'lang_id', 'route_name', 'content', 'status'];
+
+    public function getAvailableLang()
+    {
+        return SharedHelper::getAvailableLang($this);
+    }
+
+    public function langs()
+    {
+        return $this->belongsTo(Lang::class, 'lang_id');
+    }
+    public function langParent()
+    {
+        return $this->belongsTo(AssignedTitle::class, 'lang_parent_id');
+    }
+    public function langChildren()
+    {
+        return $this->hasMany(AssignedTitle::class, 'lang_parent_id');
+    }
+}
