88ca78babbe6c3669d3b3870628e17d33c0ea149
Catty <ducvu73@gmail.com>
2025-01-11 18:20:53 +0700

    feat: Adds assigned_titles migration

---
+++ database/migrations/2025_01_11_092826_assigned_titles.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\AssignedContent;
+use App\Models\AssignedTitle;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AssignedTitles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assigned_titles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('lang_parent_id')->nullable()->default(null);
+            $table->unsignedBigInteger('lang_id')->nullable()->default(null);
+            $table->string('route_name', 255);
+            $table->text('content')->nullable();
+            $table->unsignedInteger('status')->nullable()->default(AssignedTitle::STATUS_ACTIVE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
