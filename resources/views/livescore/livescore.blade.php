53d60e79a62201e99ccd6f76b1e0a658106d54b6
Catty <ducvu73@gmail.com>
2025-01-16 17:32:08 +0700

    feat: Refactor livescore view to enhance layout and add match calendar functionality

--- resources/views/livescore/livescore.blade.php
+++ resources/views/livescore/livescore.blade.php
@@ -136,7 +136,7 @@
                         </div>
                     </div> --}}
 
-                    <div class="row all-bookers">
+                    {{-- <div class="row all-bookers">
                         <div class="col">
                             <div class="list-group">
                             <div id="loading-notify" class="alert alert-info text-center mt-5" style="display: none;">
@@ -157,9 +157,6 @@
                                 <iframe src="https://www.scorebat.com/embed/livescore/?token=MTU4NTk2XzE3MjgzNTU0MzdfM2UyMmVmZDNlZTZlZjljOTI5MWIwODgwMTk1MzdkNTgwYWJjNzg3Ng==" frameborder="0" width="100%" height="760" allowfullscreen allow='autoplay; fullscreen' style="width:100%;height:1760px;overflow:show;display:block;padding-top:10px" class="_scorebatEmbeddedPlayer_ container-fluid"></iframe>
                                 <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = 'https://www.scorebat.com/embed/embed.js?v=arrv'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'scorebat-jssdk'));</script>
                             @else
-                                {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-                                    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
-                                </script> --}}
                                 </p>
                                 <p>
                                     <script type="text/javascript">
@@ -193,7 +190,35 @@
                                 @endif
                             </div>
                         </div>
+                    </div> --}}
+                    <div id="__livescore" style="overflow: auto; max-height: 95vh;">
+                        <div class="r football">
+                            <div id="app" class="A">
+                                <div class="mh" id="match-rows_calendar">
+                                    <div class="Rg">
+                                        <span class="Ug">
+                                            <a data-testid="match-calendar-live" href="#">
+                                                <div>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#fdfdfd" viewBox="0 0 30 20" width="30" height="20">
+                                                        <path d="M27 0a3 3 0 0 1 3 3v14a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V3a3 3 0 0 1 3-3h24zM5.837 6.25H3.866V14H9.13v-1.627H5.838V6.25zm6.297 0h-1.971V14h1.971V6.25zm3.167 0H13.04L15.538 14h2.492l2.433-7.75h-2.127l-1.445 5.704h-.102L15.301 6.25zm11.379 0h-5.312V14h5.312v-1.584h-3.341v-1.584h3.142v-1.45h-3.142V7.835h3.341V6.251z"/>
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                        </span>
+                                        <div class="Zg" id="calendar-dates"></div>
+                                        <div class="oh" id="match-calendar-dp-trigger">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="#fdfdfd" viewBox="0 0 20 20" width="18" height="18">
+                                                <path d="M5.556 0v1.111h8.889V0h1.111v1.111h1.667a2.778 2.778 0 0 1 2.771 2.587v.009l.006.182v13.333A2.778 2.778 0 0 1 17.222 20H2.778A2.778 2.778 0 0 1 0 17.222V3.889a2.778 2.778 0 0 1 2.778-2.778h1.667V0h1.111zM4.444 2.222H2.777A1.67 1.67 0 0 0 1.11 3.889v13.333a1.67 1.67 0 0 0 1.667 1.667h14.444a1.67 1.67 0 0 0 1.667-1.667V3.889a1.67 1.67 0 0 0-1.667-1.667h-1.667v1.111h-1.111V2.222H5.554v1.111H4.443V2.222zm12.778 14.445H2.778V5.556h14.444v11.111zM6.111 13.333H3.889v2.222h2.222v-2.222zm3.333 0H7.222v2.222h2.222v-2.222zm3.334 0h-2.222v2.222h2.222v-2.222zm3.333 0h-2.222v2.222h2.222v-2.222zM6.111 10H3.889v2.222h2.222V10zm3.333 0H7.222v2.222h2.222V10zm3.334 0h-2.222v2.222h2.222V10zm3.333 0h-2.222v2.222h2.222V10zm-10-3.333H3.889v2.222h2.222V6.667zm3.333 0H7.222v2.222h2.222V6.667zm3.334 0h-2.222v2.222h2.222V6.667zm3.333 0h-2.222v2.222h2.222V6.667z"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="matches-container"></div>
+                            </div>
+                        </div>
                     </div>
+                    <script src="https://cdn.bettests.com/live-main/livescore.js"></script>
+
                 </div>
             </div>
         </div>
